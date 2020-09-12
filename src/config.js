d815d03cc1d77dd26f4a7cd8fe74077f467a28e4
Carlos Bello <bellocarlos2810@gmail.com>
2020-09-11 21:15:04 -0500

    config url api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const urlApiApp = 'http://198.211.113.192/app/'
+export const urlApiApp = 'http://198.211.113.192/'
 // export const urlApiApp = 'http://localhost:3000/'
 export const urlApiAuth = 'http://198.211.113.192/auth/'
